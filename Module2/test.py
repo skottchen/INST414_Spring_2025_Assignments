461fec430d1553c00b3b2258cdb256bfa694f43b
Scott Chen <schen78997@gmail.com>
2025-03-10 16:39:34 -0400

    Delete Module2/test.py

--- Module2/test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-file_name = "artists_1_to_10.json"  # Replace with your actual file
-with open(file_name, "r") as f:
-    try:
-        json_data = json.load(f)
-        print("Valid JSON")
-    except json.JSONDecodeError as e:
-        print(f"Invalid JSON: {e}")
